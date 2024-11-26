60db763fd1b12a1cd2fa86b0f25ac68858a6b22d
taketori <naoki123453@gmail.com>
2024-11-26 20:52:04 +0900

    リンクの追加

--- src/App.tsx
+++ src/App.tsx
@@ -106,6 +106,7 @@ function App() {
         </div>
       </main>
       <footer>
+        <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E7%AB%B9%E5%8F%96%E7%BF%81/%E5%A4%9A%E5%A4%89%E6%95%B0%E4%BA%9E%E9%96%A2%E6%95%B0%E3%81%8B%E3%82%89Buchholz_OCF%E3%81%AB%E4%BC%B4%E3%81%86%E9%A0%86%E5%BA%8F%E6%95%B0%E8%A1%A8%E8%A8%98%E3%81%B8%E3%81%AE%E5%A4%89%E6%8F%9B%E5%86%99%E5%83%8F" target="_blank" rel="noreferrer">ユーザーブログ:竹取翁/多変数亞関数からBuchholz OCFに伴う順序数表記への変換写像 | 巨大数研究 Wiki | Fandom</a>(2024/11/26 閲覧)<br />
         <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:%E7%AB%B9%E5%8F%96%E7%BF%81/%E5%A4%9A%E5%A4%89%E6%95%B0%E4%BA%9E%E9%96%A2%E6%95%B0" target="_blank" rel="noreferrer">ユーザーブログ:竹取翁/多変数亞関数 | 巨大数研究 Wiki | Fandom</a>(2024/11/24 閲覧)<br />
         <a href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:P%E9%80%B2%E5%A4%A7%E5%A5%BD%E3%81%8Dbot/%E6%8B%A1%E5%BC%B5Buchholz_OCF%E3%81%AB%E4%BC%B4%E3%81%86%E9%A0%86%E5%BA%8F%E6%95%B0%E8%A1%A8%E8%A8%98" target="_blank" rel="noreferrer">ユーザーブログ:P進大好きbot/拡張Buchholz OCFに伴う順序数表記 | 巨大数研究 Wiki | Fandom</a>(2024/11/24 閲覧)<br />
         このページは<a href="https://creativecommons.org/licenses/by-sa/3.0/legalcode" target="_blank" rel="noreferrer">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>の下に公開されます。<br />
